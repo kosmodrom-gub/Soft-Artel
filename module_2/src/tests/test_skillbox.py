0234610ddeb6e88c6bd9621e2c7dd222edcfc3c9
kosmodrom-gub <kosmodrom@soft-artel.com>
2025-09-12 16:27:56 +0300

    Add files via upload
    59: Разработка спецификации Главный экран

---
+++ module_2/src/tests/test_skillbox.py
@@ -0,0 +1,23 @@
+import pytest
+
+def test_stub():
+    """Заглушка для теста."""
+    pass
+
+def test_sklibox_title():
+    """Проверяет заголовок главной страницы skillbox.ru"""
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+    from selenium.webdriver.chrome.options import Options
+
+
+    options = Options()
+    options.add_argument('--headless')  # Без интерфейса браузера
+    driver = webdriver.Chrome(options=options)
+
+    try:
+        driver.get('https://skillbox.ru')
+        title = driver.title
+        assert "Skillbox" in title
+    finally:
+        driver.quit()
